2344a4c6ef452ee696ed527eb110d22db92789aa
Varun Kashyap <20821711+Varun9729@users.noreply.github.com>
2022-01-05 14:28:41 -0800

    Completed Destinii challenge

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+struct Story
+{
+    let title:String
+    let choice1:String
+    let choice2:String
+    let choice1Destination:Int
+    let choice2Destination:Int
+    
+    init(t:String, c1:String, c2:String, cd1:Int, cd2:Int)
+    {
+        title = t
+        choice1 = c1
+        choice2 = c2
+        choice1Destination = cd1
+        choice2Destination = cd2
+    }
+}
